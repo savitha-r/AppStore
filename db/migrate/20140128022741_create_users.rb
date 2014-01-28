782179882655d05f5180e5986766fef29afc125e
savitha-r <savitha.r@2359media.com>
2014-01-28 08:01:59 +0530

    add basic user validation and default value

--- db/migrate/20140128022741_create_users.rb
+++ db/migrate/20140128022741_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :oauth_token
-      t.boolean :is_admin
+      t.boolean :is_admin, :default => false
 
       t.timestamps
     end
